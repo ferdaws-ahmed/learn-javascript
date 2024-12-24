d0008a6c27b862bcbc7225670844ec0b2b934ac2
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-24 12:04:04 +0600

    update

--- js problem solving/var-swap.js
+++ js problem solving/var-swap.js
@@ -1,5 +1,8 @@
 
 
+    // vairable swap>>
+
+//1st method:
 var a = 10;
 var b = 20;
 console.log( "before swap: a = ", a, ",", " b = ", b);
@@ -7,3 +10,25 @@ var temp = b;
 b = a;
 var a = temp;
 console.log("after swap: a =",a,","," b = ",b);
+
+// 2nd method:
+var  x = 10;
+var  y = 20;
+console.log ("befor swap: x = ", x, " y = ",y);
+
+var x =10;
+var y =20;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("after swap: x = ",x, " y = ",y);
+
+
+
+// 3rd method:
+
+var p = 10;
+var q = 20;
+
+[p, q] = [q, p];
+console.log("after swap : p = ",p, "q = ",q);
