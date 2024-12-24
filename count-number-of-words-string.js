7d27e6660337867e9ee6a4f7eb1d4ca50c697308
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-24 16:33:27 +0600

    update

---
+++ count-number-of-words-string.js
@@ -0,0 +1,39 @@
+
+
+var sentence = "i am a puck boy. i can  puck any body every single time."
+
+count = 0;
+for (var i = 0; i <sentence.length; i++){
+    var char = sentence[i];
+    if(char == " " && sentence[i - 1] != " "){
+       count++;
+    }
+     
+}
+
+console.log(count);
+
+
+
+
+
+
+
+
+
+
+
+
+//practice 1:
+
+var speech = "Bangladesh is my won country. it is very beautiful.  in this country have may puck boy.";
+
+number = 0;
+for( var i = 0; i < speech.length; i++){
+    var char = speech[i];
+    if(char == " " && speech[i - 1] != " "){
+        number++;
+    }
+}
+number++;
+console.log(number);
