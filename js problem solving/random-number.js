c8a63bf0333dded5f8f61bb32d072cbd02622830
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-24 12:30:19 +0600

    update

--- js problem solving/random-number.js
+++ js problem solving/random-number.js
@@ -1,1 +1,12 @@
 
+
+
+
+
+// using for loop and generate random number.
+var num = 1
+for( var i = 1; i < 7; i++){
+    var result = Math.random(num);
+    var result2 = Math.round(result * 6);
+    console.log(result2);
+}
