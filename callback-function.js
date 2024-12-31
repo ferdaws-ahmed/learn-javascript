d2b8b3ff5ca709b080f9fcd24a46667775b4a2ea
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-31 09:03:57 +0600

    update

--- callback-function.js
+++ callback-function.js
@@ -3,7 +3,18 @@
 function callBack (name, age, task){
     console.log('Your Name is ',name);
     console.log('Your Age is', age);
+    task()
 }
-callBack('pokom', 15);
-callBack('mokom', 22);
-callBack('tokom', 19);
+
+function washHand(){
+    console.log('Wash your hand first');
+}
+function gosolKoro(){
+    console.log('Tumi gosol koro taratari');
+}
+function vatKhaw(){
+    console.log('Vat khaw siggir. Noile mair ase kopale');
+}
+callBack('pokom', 15, washHand);
+callBack('mokom', 22, gosolKoro);
+callBack('tokom', 19, vatKhaw);
