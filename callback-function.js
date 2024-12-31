b14f9a9bdf4480574cc9db951582bf1701cd6d31
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-31 08:58:41 +0600

    update

--- callback-function.js
+++ callback-function.js
@@ -4,3 +4,6 @@ function callBack (name, age, task){
     console.log('Your Name is ',name);
     console.log('Your Age is', age);
 }
+callBack('pokom', 15);
+callBack('mokom', 22);
+callBack('tokom', 19);
